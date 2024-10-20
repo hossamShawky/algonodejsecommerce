66787ed9b533b7965183d9cd6c601d49db78ebd8
Hossam M.Shawky <52916569+hossamShawky@users.noreply.github.com>
2024-10-20 11:30:24 +0300

    native connect

--- packages/evershop/src/lib/postgres/connection.js
+++ packages/evershop/src/lib/postgres/connection.js
@@ -83,7 +83,12 @@ const { getConfig } = require('../util/getConfig');
 // const ca = Buffer.from(process.env.DB_SSLROOTCERT, 'base64').toString('utf8');
 
 const connectionSetting = {
-  connectionString: process.env.DATABASE_URL,
+  // connectionString: process.env.DATABASE_URL,
+    host: process.env.DB_HOST ,
+  port: process.env.DB_PORT  ,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD  ,
+  database: process.env.DB_NAME ,
   ssl: {
     rejectUnauthorized: false,
     // ca: ca,
